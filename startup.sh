913c743118cb1af5eb0aa0373dd6f4294eac950c
percent73 <6688327+geziang@users.noreply.github.com>
2022-07-25 14:49:01 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-envsubst < /root/config.json.tp > /usr/local/etc/v2ray/config.json
-
 # Download and install V2Ray
 mkdir /tmp/v2ray
 curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
@@ -14,6 +12,54 @@ rm -rf /tmp/v2ray
 
 # V2Ray new configuration
 install -d /usr/local/etc/v2ray
+
+cat <<-EOF > /usr/local/etc/v2ray/config.json
+{
+    "policy": {
+        "levels": {
+            "0": {
+                "handshake": 5,
+                "connIdle": 300,
+                "uplinkOnly": 2,
+                "downlinkOnly": 5,
+                "statsUserUplink": false,
+                "statsUserDownlink": false,
+                "bufferSize": 10240
+            }
+        },
+        "system": {
+            "statsInboundUplink": false,
+            "statsInboundDownlink": false,
+            "statsOutboundUplink": false,
+            "statsOutboundDownlink": false
+        }
+    },
+    "inbounds": [
+        {
+            "port": ${PORT},
+            "protocol": "vless",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "${UUID}",
+                        "level": 0
+                    }
+                ],
+                "decryption": "none"
+            },
+            "streamSettings": {
+                "network": "ws",
+                "security": "none"
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        }
+    ]
+}
+EOF
 
 # Run
 if [[ $TUNNEL_TOKEN ]]; then
