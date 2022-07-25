38b83d49342067ec4230b5b2bf2aa1b9fadeb10d
percent73 <6688327+geziang@users.noreply.github.com>
2022-07-25 14:28:33 +0800

    Add files via upload

---
+++ startup.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+envsubst < /root/config.json.tp > /usr/local/etc/v2ray/config.json
+
+# Download and install V2Ray
+mkdir /tmp/v2ray
+curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
+unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
+install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
+install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
+
+# Remove temporary directory
+rm -rf /tmp/v2ray
+
+# V2Ray new configuration
+install -d /usr/local/etc/v2ray
+
+# Run
+if [[ $TUNNEL_TOKEN ]]; then
+echo 'has tunnel token, run cloudflared tunnel'
+wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 -O /root/cloudflared
+chmod +x /root/cloudflared
+/usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json & /root/cloudflared tunnel --no-autoupdate run --token $TUNNEL_TOKEN --protocol http2 --url localhost:$PORT
+else
+/usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json
+fi
+
